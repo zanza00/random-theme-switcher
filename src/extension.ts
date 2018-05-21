b8323e3502352d614d0b1da3eb2c5981a8c9c871
Simone Picciani <zanza00@gmail.com>
2018-05-21 23:39:45 +0200

    first version of startup change of theme

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,46 @@
 import * as vscode from "vscode";
 import { random } from "lodash";
 
+function getSettings(): {
+  userSettings: vscode.WorkspaceConfiguration;
+  extensionConfig: vscode.WorkspaceConfiguration;
+} {
+  const userSettings = vscode.workspace.getConfiguration();
+  const extensionConfig = vscode.workspace.getConfiguration("randomThemeSwitcher");
+  return { userSettings, extensionConfig };
+}
+
+function changeTheme(userSettings: vscode.WorkspaceConfiguration, extensionConfig: vscode.WorkspaceConfiguration) {
+  const themeList = getThemeList(extensionConfig);
+  const i = random(themeList.length - 1);
+  const newTheme = themeList[i];
+  userSettings.update("workbench.colorTheme", newTheme, true);
+  vscode.window.showInformationMessage(`Theme switched to ${newTheme}`);
+}
+
+function getThemeList(extensionConfig: vscode.WorkspaceConfiguration): string[] {
+  let themeList: string[] | undefined = extensionConfig.get("themeList");
+  if (themeList === undefined || themeList.length === 0) {
+    themeList = ["Default Dark+", "Default Light+"];
+  }
+  return themeList;
+}
+
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("randomThemeSwitcher.switchTheme", () => {
-    const userSettings = vscode.workspace.getConfiguration();
-    const extensionConfig = vscode.workspace.getConfiguration("randomThemeSwitcher");
+  let switchTheme = vscode.commands.registerCommand("randomThemeSwitcher.switchTheme", () => {
+    const { extensionConfig, userSettings } = getSettings();
 
-    let themeList: string[] | undefined = extensionConfig.get("themeList");
-    if (themeList === undefined || themeList.length === 0) {
-      themeList = ["Default Dark+", "Default Light+"];
-    }
-    console.log("themeList", themeList);
-
-    const i = random(themeList.length - 1);
-    userSettings.update("workbench.colorTheme", themeList[i], true);
-    vscode.window.showInformationMessage(`Theme switched to ${themeList[i]}`);
+    changeTheme(userSettings, extensionConfig);
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(switchTheme);
+  const { extensionConfig, userSettings } = getSettings();
+
+  const isActive = extensionConfig.get("switchOnStart");
+
+  if (isActive) {
+    changeTheme(userSettings, extensionConfig);
+  }
 }
 
 // this method is called when your extension is deactivated
